82cd2abd21bea717640460d8c6b959ad5cb103c9
tim <timothy.daniel.roberts@gmail.com>
2013-11-15 00:38:25 +0000

    finished nd_impulse method

--- signal_utilities/sig_utils.py
+++ signal_utilities/sig_utils.py
@@ -3,6 +3,8 @@
 a collection of matrix utilities
 """
 import numpy as np
-def nd_impulse(ary_size)
+
+def nd_impulse(ary_size):
     ary_impulse = np.zeros(ary_size)
-    ary_size/2+1
+    ary_impulse[tuple(np.array(ary_impulse.shape)/2)] = 1
+    return ary_impulse
