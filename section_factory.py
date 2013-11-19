36b44b4f416f1bca71c41d20f4097ddbe9bc6efe
tim <timothy.daniel.roberts@gmail.com>
2013-11-19 18:04:06 +0000

    added section factory, sig_utils

---
+++ section_factory.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python -tt
+from __future__ import generators
+from py_operators import *
+import random
+
+class SectionFactory(object):
+    """
+    Factory for creating  objects derived from Section.
+    """
+    factories = {}
+    
+    def add_factory(str_id, sec_factory):
+        SectionFactory.factories[str_id] = sec_factory
+    add_factory = staticmethod(add_factory)
+    
+    def create_section(ps_parameters,str_section):
+        str_id = ps_parameters.get_section_dict(str_section)['name']
+        if not SectionFactory.factories.has_key(str_id):
+            SectionFactory.factories[str_id] = \
+              eval(str_id + '.Factory()')
+        return SectionFactory.factories[str_id]. \
+               create(ps_parameters,str_section)
+    create_section = staticmethod(create_section)
--- signal_utilities/sig_utils.py
+++ signal_utilities/sig_utils.py
@@ -2,7 +2,10 @@
 """
 a collection of signal/matrix utility functions
 """
+from operator import add
 import numpy as np
+from numpy import max, absolute
+import itertools
 
 def nd_impulse(ary_size):
     ary_impulse = np.zeros(ary_size)
@@ -12,25 +15,43 @@ def nd_impulse(ary_size):
 def spectral_radius(op_transform, op_modality):
     """Compute the specral radius including crossband energy
     :param op_transform: a transform operator which returns a ws object
-    :param op_modality: some linear operator
+    :param op_modality: some linear operator, 
+     assumes this is diagonalizable by the fft (e.g. convolution)
 
     :returns ary_lambda_alpha, a vector of subband weights which upperbounds the spectral radius
     
     .. codeauthor:: Timothy Roberts <timothy.daniel.roberts@gmail.com>, 2013
     """
     ary_impulse = nd_impulse(op_transform.ary_size)
+    ls_sp = []
+    ls_sp_conj = []
     ws_w = op_transform * ary_impulse
-    for int_subband in np.arange(ws_w.int_subbands):
-        np.fftn(~op_transform * ws_w.suppress_other_subbands(int_subband))
-    
+    ary_alpha = np.zeros(ws_w.int_subbands)
+    ary_ms = op_modality.get_spectrum
+    ary_ms_conj = np.conj(ary_ms)
+    ary_subbands = np.arange(ws_w.int_subbands)
+    #store the inband psd's (and conjugate)
+    for int_subband in ary_subbands:
+        ary_subband_spectrum = np.fftn(~op_transform * \
+                                          ws_w.suppress_other_subbands(int_subband))
+        ls_sp.append(ary_ms * ls_subband_spectra[int_subband])
+        ls_sp_conj.append(ary_ms_conj *ls_subband_spectra[int_subband])
+    #compute the inband and crossband psd maxima
+    ls_ic = np.array([max(absolute(ls_sp[int_j] * ls_sp[int_n])) for \
+                      int_j in ary_subbands for int_n in ary_subbands])
+    ary_alpha = np.array([reduce(add,ls_ic[i:i+ary_subbands[-1]]) for \
+                          i in (ary_subbands*ary_subbands[-1]+1)])
+    #return the alpha array, upper-bounded to 1                      
+    return np.minimum(ary_alpha,1)
+
 def circshift(ary_input, tup_shifts):
     """Shift array circularly.
   
     Circularly shifts the values in the array `a` by `s`
     elements. Return a copy.
   
-    Parameters
-    ----------
+    Parameters!23Gamma
+    
     ary_input : ndarray to shift.
   
     s : tuple of int
