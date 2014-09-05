updates, subband rel error still incorrect computation, and there is a bug in preprocess for global phase correction

results/met_subband_rel_error.py
@@ -24,7 +24,7 @@ class SRE(SubbandMetric):
             self.w = dict_in['w'][self.real_imag]
             self.get_legend_info(self.w)
             self.subband_energies = [norm(self.w.get_subband(j).flatten(),2)**2 for j in xrange(self.w.int_subbands)]
-        value = np.array([((norm((self.w.get_subband(j) - dict_in['w_n'][0].get_subband(j)).flatten(),2)**2)/
+        value = np.array([((norm((self.w.get_subband(j) - dict_in['w_n'][self.real_imag].get_subband(j)).flatten(),2)**2)/
                            self.subband_energies[j]) for j in xrange(self.w.int_subbands)])
         super(SRE,self).update(value)
         

sec_observe.py
@@ -139,9 +139,10 @@ class Observe(Section):
                 DHt = conj(DH)
                 Hty=fftn(D*(~Phi * dict_in['y']))
                 HtDtDH=np.real(DHt*DH)
-                dict_in['x_0'] = ~D*real(ifftn(Hty /
-                                               (HtDtDH + 
-                                                wrf * noise_pars['variance'])))
+                # dict_in['x_0'] = ~D*real(ifftn(Hty /
+                #                                (HtDtDH + 
+                #                                 wrf * noise_pars['variance'])))
+                dict_in['x_0'] = ~D * dict_in['y']
                 #optional interpolation
                 xdim=dict_in['x'].ndim
                 if self.get_val('interpinitialsolution',True) and xdim < 3:

sec_preprocess.py
@@ -116,7 +116,8 @@ class Preprocess(Section):
             dict_global_lims['upperlimit'] = self.get_val('phaseupperlimit',True)
             dict_global_lims['boundary_mask'] = dict_in['boundarymask']
             dict_global_lims['boundary_upperlimit'] = self.get_val('boundaryphaseupperlimit',True)
-                
+            # pdb.set_trace()
+            
             theta = phase_unwrap(theta, dict_global_lims, ls_local_lim_secs)
             magnitude /= np.max(nabs(magnitude))
             dict_in['x'] = magnitude*exp(1j*theta)